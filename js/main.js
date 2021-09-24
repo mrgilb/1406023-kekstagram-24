f96da6f18029f6cb302bf51a69811bc458ab5158
mrgilb <ilya.nikolenko.job@gmail.com>
2021-09-24 23:04:12 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ function getRandomIntInclusive(min, max) {
     if (min>max) {
       min = [max, max = min][0]; // https://coderoad.ru/16201656/%D0%9A%D0%B0%D0%BA-%D0%BF%D0%BE%D0%BC%D0%B5%D0%BD%D1%8F%D1%82%D1%8C-%D0%BC%D0%B5%D1%81%D1%82%D0%B0%D0%BC%D0%B8-%D0%B4%D0%B2%D0%B5-%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B2-JavaScript
     }
+
+    if (min === max) {
+      return false;
+    }
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //MDN
@@ -11,11 +15,11 @@ function getRandomIntInclusive(min, max) {
 
 getRandomIntInclusive(5, 1);
 
-function checksString (checkedString, maxLenght) {
-  if (checkedString.length >= maxLenght) {
-    return true;
+function checksString (checkedString, maxLength) {
+  if (checkedString.length >= maxLength) {
+    return false;
   }
-  return false;
+  return true;
 }
 
 checksString('asdassssd', 5);
